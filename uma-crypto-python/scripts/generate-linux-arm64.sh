3442a65952df00acbae3f53eb6151c1eedaf0e11
Zhen Lu <ne.rwa.lz@gmail.com>
2023-10-23 14:05:54 -0700

    Build arch64 for uma-crypto python (#1)
    * Build arch64 for uma-crypto
    
    * Add build script

---
+++ uma-crypto-python/scripts/generate-linux-arm64.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+python --version
+pip install -r requirements.txt
+
+echo "Generating python file..."
+cd ..
+cargo run --bin uniffi-bindgen generate src/uma_crypto.udl --language python --out-dir uma-crypto-python/src/uma_crypto/ --no-format
+
+echo "Generating native binaries..."
+docker buildx build -f build.Dockerfile --platform linux/arm64 -o docker-out .
+
+echo "Copying linux binary..."
+cp docker-out/target/aarch64-unknown-linux-gnu/release-smaller/uma_crypto.so uma-crypto-python/src/uma_crypto/libuniffi_uma_crypto.so
+
+echo "All done!"
